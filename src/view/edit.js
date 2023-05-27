9fef5e73d7336f02938cac7b4a8da72cf4f9ac66
Sangam Rimal <sangamr.official@gmail.com>
2023-05-27 15:48:53 +0545

    updated

--- src/view/edit.js
+++ src/view/edit.js
@@ -7,6 +7,61 @@ const Edit = () => {
     const { id } = useParams()
     const [product, setProduct] = useState({})
     const [loader, setLoader] = useState(false)
+
+    const handleTitleChange = (event) => {
+        console.log(event.target.value)
+        setProduct({ ...product, title: event.target.value });
+    }
+
+    const handleDescriptionChange = (event) => {
+        setProduct({ ...product, description: event.target.value });
+    }
+
+    const handlePriceChange = (event) => {
+        setProduct({ ...product, price: event.target.value });
+    }
+
+    const handleCategoryChange = (event) => {
+        setProduct({ ...product, category: event.target.value });
+    }
+
+    const handleImageChange = (event) => {
+        setProduct({ ...product, image: event.target.value });
+    }
+
+    const updateProduct = async () => {
+        try {
+            await fetch(`https://fakestoreapi.com/products/${product.id}`, {
+                method: "PATCH",
+                body: JSON.stringify(
+                    product
+                )
+            })
+                .then(res => res.json())
+                .then(json => console.log(json))
+        } catch (error) {
+            console.log('error is :', error)
+        }
+    }
+
+    const clearform=()=>{
+        setProduct({
+            title: '',
+            description: '',
+            price: 0,
+            category: '',
+            image: '',
+        }) 
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        console.log('updated')
+        await updateProduct()
+        //clear form
+        clearform()
+
+    }
     const getProduct = async () => {
         setLoader(true);
         try {
@@ -27,48 +82,47 @@ const Edit = () => {
     /*==============header=============*/
         <Header />
         <div className='container body-margin'>
-            {loader ? <Loader /> : <Body product={product} />}
+            {
+                loader ? <Loader /> :
+                    <div className='card'>
+                        <div className='card-header d-flex justify-content-between pb-0'>
+                            <h2 className='header-title m-0'>Update Product</h2>
+                            <Link to={'/'}><button className='btn btn-outline-success float-end'>All Products</button></Link>
+                        </div>
+                        <div className='card-body'>
+                            <form onSubmit={handleSubmit}>
+                                <div className='row'>
+                                    <div className='col col-12 mb-2'>
+                                        <label className='form-label'>Title</label>
+                                        <input placeholder='title...' onChange={handleTitleChange} value={product.title} className='form-control' name='title' />
+                                    </div>
+                                    <div className='col col-12 mb-2'>
+                                        <label className='form-label'>Description</label>
+                                        <textarea placeholder='desc...' onChange={handleDescriptionChange} value={product.description} rows='6' className='form-control' name='description' />
+                                    </div>
+                                    <div className='col col-6 mb-2'>
+                                        <label className='form-label'>Price</label>
+                                        <input placeholder='price...' onChange={handlePriceChange} value={product.price} type={'number'} className='form-control' name='price' />
+                                    </div>
+                                    <div className='col col-6 mb-2'>
+                                        <label className='form-label'>Category</label>
+                                        <input type='text' onChange={handleCategoryChange} value={product.category} className='form-control' name='category' />
+                                    </div>
+                                    <div className='col col-6 mb-2'>
+                                        <label className='form-label'>Image</label>
+                                        <input placeholder='image url' onChange={handleImageChange} value={product.image} className='form-control' name='image' />
+                                    </div>
+                                    <div className='col col-12'>
+                                        <button type='submit' className='btn btn-outline-primary'>Create</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+            }
         </div>
     </>
     )
 }
 
 export default Edit
-
-export const Body = ({ product }) => {
-    return (
-        <div className='card'>
-            <div className='card-header d-flex justify-content-between pb-0'>
-                <h2 className='header-title m-0'>Update Product</h2>
-                <Link to={'/'}><button className='btn btn-outline-success float-end'>All Products</button></Link>
-            </div>
-            <div className='card-body'>
-                <div className='row'>
-                    <div className='col col-12 mb-2'>
-                        <label className='form-label'>Title</label>
-                        <input placeholder='title...' className='form-control' name='title' value={product.title} />
-                    </div>
-                    <div className='col col-12 mb-2'>
-                        <label className='form-label'>Description</label>
-                        <textarea placeholder='desc...' rows='6' className='form-control' name='description' value={product.description} />
-                    </div>
-                    <div className='col col-6 mb-2'>
-                        <label className='form-label'>Price</label>
-                        <input placeholder='price...' type={'number'} className='form-control' name='price' value={product.price} />
-                    </div>
-                    <div className='col col-6 mb-2'>
-                        <label className='form-label'>Category</label>
-                        <input type={'text'} className='form-control' name='price' value={product.category} />
-                    </div>
-                    <div className='col col-6 mb-2'>
-                        <label className='form-label'>Image</label>
-                        <input placeholder='image url' className='form-control' name='image' value={product.image} />
-                    </div>
-                    <div className='col col-12'>
-                        <button className='btn btn-outline-primary'>Update</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
